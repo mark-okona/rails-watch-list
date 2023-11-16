f249ba1200b97644ce92f98b0163aaa8cb660c45
Mark O'Connor <markoconnor1000@gmail.com>
2023-11-16 13:29:27 +0000

    model rake finally green

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,6 @@
 class Bookmark < ApplicationRecord
-  belongs_to :Movie
-  belongs_to :List
+  belongs_to :movie
+  belongs_to :list
+  validates :comment, length: { minimum: 6 }
+  validates :movie_id, uniqueness: { scope: :list_id }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_16_112416) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_16_132553) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookmarks", force: :cascade do |t|
+    t.text "comment"
+    t.bigint "movie_id", null: false
+    t.bigint "list_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["list_id"], name: "index_bookmarks_on_list_id"
+    t.index ["movie_id"], name: "index_bookmarks_on_movie_id"
+  end
 
   create_table "lists", force: :cascade do |t|
     t.string "name"
@@ -24,9 +34,11 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_16_112416) do
     t.string "title"
     t.text "overview"
     t.string "poster_url"
-    t.decimal "rating"
+    t.float "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "bookmarks", "lists"
+  add_foreign_key "bookmarks", "movies"
 end

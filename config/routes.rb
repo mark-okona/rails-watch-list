3e2c56b6b79eb1787f43c42851bd919e356453b0
Mark O'Connor <markoconnor1000@gmail.com>
2023-11-16 14:47:20 +0000

    list model & controller working

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  get "lists", to: "lists#index"
+  get "lists/new", to: "lists#new"
+  post "lists", to: "lists#create"
+  get "lists/:id", to: "lists#show", as: "list"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,19 @@
 require "open-uri"
 require"json"
 
+Movie.destroy_all
+List.destroy_all
+
 filepath = "https://tmdb.lewagon.com/movie/top_rated"
 serialized_movie_list = URI.open(filepath).read
 movie_list = JSON.parse(serialized_movie_list)
 
 movie_list['results'].each do |movie|
   Movie.create(title: movie['original_title'], overview: movie['overview'], poster_url: movie['poster_path'], rating: movie['vote_average'])
+end
+
+list_name_array = ["Crime", "Good", "Bad", "Tense", "Chill", "Criterion", "Mubi", "New" ]
+
+list_name_array.each do |item|
+  List.create(name: item)
 end

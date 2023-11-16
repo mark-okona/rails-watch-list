c06de5ec9853437d421329e6230b322b1c615356
Mark O'Connor <markoconnor1000@gmail.com>
2023-11-16 16:28:02 +0000

    add bookmark working

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,20 @@
+class BookmarksController < ApplicationController
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.save
+    # No need for app/views/restaurants/create.html.erb
+    redirect_to list_path(@list)
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
+  end
+end
